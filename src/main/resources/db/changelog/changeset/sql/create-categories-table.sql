b141e841cb3126058dbadfb98de16fee5efaca6d
Denis <neothat@mail.ru>
2022-09-29 00:53:19 +0300

    Application configuration, liquibase and database initialization

---
+++ src/main/resources/db/changelog/changeset/sql/create-categories-table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS rs24.rs24schem.categories
+(
+    id                bigserial primary key,
+    name              varchar,
+    short_description text
+);
---
+++ src/main/resources/db/changelog/changeset/sql/init-products-table.sql
@@ -0,0 +1,9 @@
+INSERT INTO rs24.rs24schem.products (name, description, price, image, category, date_added, status)
+VALUES ('Pomodoro', 'An annual or perennial herb, a species of the Solanum genus of the Solanaceae family', 86, 'path', 1, 1664400414514, true),
+       ('Potato', 'A species of perennial tuberous herbaceous plants from the genus Nightshade of the Solanaceae family', 30, 'path', 1, 1664400542309, true),
+       ('Apple', 'The juicy fruit of the apple tree, which is eaten fresh and baked', 99, 'path', 2, 1664400606934, true),
+       ('Banana', 'A tall tropical plant with very large leaves, as well as its sweet mealy fruit', 66, 'path', 2, 1664400821588, true),
+       ('Macaroni', 'A food product made from dried dough in the form of long tubes', 68, 'path', 3, 1664401475587, true),
+       ('Rice', 'Cereal with white oblong grains for food, as well as its grains', 113, 'path', 3, 1664401587704, true),
+       ('Chips', 'Crispy potatoes fried in vegetable oil', 132, 'path', 4, 1664401699081, true),
+       ('Crackers', 'Cut and dried bakery product with a moisture content of 8-12%', 22, 'path', 4, 1664401822869, true);
