99bb9d68e4c2f408d22274d6ae75faef69c1ea45
Denis <neothat@mail.ru>
2022-10-03 01:39:42 +0300

    Minimum front to display products with filtering

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,20 @@
+angular.module('app', []).controller('indexController', function ($scope, $http) {
+    const contextPath = 'http://localhost:8080/api/v1';
+
+    $scope.loadProducts = function (pageIndex = 1) {
+        $http({
+            url: contextPath + '/products',
+            method: 'GET',
+            params: {
+                category: $scope.filter ? $scope.filter.category : null,
+                name_part: $scope.filter ? $scope.filter.name_part : null,
+                min_price: $scope.filter ? $scope.filter.min_price : null,
+                max_price: $scope.filter ? $scope.filter.max_price : null
+            }
+        }).then(function (response) {
+            $scope.ProductsPage = response.data;
+        });
+    };
+
+    $scope.loadProducts();
+});
