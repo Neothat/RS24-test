12a31431c06e84b6fd227e24466591f9bccecf65
Denis <neothat@mail.ru>
2022-10-03 13:53:07 +0300

    Configured swagger

--- src/main/java/ru/rs24/restapi/mappers/dtos/ProductDto.java
+++ src/main/java/ru/rs24/restapi/mappers/dtos/ProductDto.java
@@ -1,5 +1,6 @@
 package ru.rs24.restapi.mappers.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,15 +10,24 @@ import javax.validation.constraints.NotEmpty;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Информация о продукте")
 public class ProductDto {
+    @Schema(description = "id", required = true)
     private long id;
     @NotEmpty
+    @Schema(description = "Имя", required = true)
     private String name;
+    @Schema(description = "Описание")
     private String description;
+    @Schema(description = "Цена")
     private int price;
+    @Schema(description = "Ссылка на изображение")
     private String image;
     @NotEmpty
+    @Schema(description = "Категория", required = true)
     private String category;
+    @Schema(description = "Дата создания")
     private long dateAdded;
+    @Schema(description = "Активность")
     private boolean status;
 }
